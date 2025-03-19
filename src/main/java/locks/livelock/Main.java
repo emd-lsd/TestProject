591b8e41b76af9ca094cef8a8752c3d069fa28eb
emd-lsd <k9514833133@gmail.com>
2025-03-19 23:28:41 +0300

    TestProject: Another livelock

---
+++ src/main/java/locks/livelock/Main.java
@@ -0,0 +1,12 @@
+package main.java.locks.livelock;
+
+/**
+ * Точка входа в программу, создает экземпляр класса переговоров.
+ * Запускает переговоры.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Negotiation negotiation = new Negotiation();
+        negotiation.startNegotiation();
+    }
+}
