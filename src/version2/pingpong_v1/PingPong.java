aa3488938f43eeec9fb983d85b4bc0bf8fe2a1ed
KASPRUK Dmitry <ruakads@n-msk-mackads.raiffeisen.ru>
2025-03-19 11:10:56 +0300

    Initial commit

---
+++ src/version2/pingpong_v1/PingPong.java
@@ -0,0 +1,56 @@
+package version2.pingpong_v1;
+
+public class PingPong {
+    private static final Object lock = new Object();
+    private static boolean pingTurn = true;
+
+    public static void main(String[] args) {
+        Thread pingThread = new Thread(new Ping());
+        Thread pongThread = new Thread(new Pong());
+
+        pingThread.start();
+        pongThread.start();
+    }
+
+    static class Ping implements Runnable{
+
+        @Override
+        public void run() {
+            for (int i = 0; i< 15; i++) {
+                synchronized (lock) {
+                    while (!pingTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    System.out.println("Ping");
+                    pingTurn = false; // Передает очередь pong
+                    lock.notifyAll(); // Разблокирует поток Pong
+                }
+            }
+        }
+    }
+
+    static class Pong implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 15; i++) {
+                synchronized (lock) {
+                    while (pingTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    System.out.println("Pong");
+                    pingTurn = true; // Передает очередь ping
+                    lock.notifyAll(); // Разблокирует поток Ping
+                }
+            }
+        }
+    }
+}
