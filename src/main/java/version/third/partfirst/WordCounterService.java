dcb1b5ae8fb79a832b92ec68e699a31fd831a934
emd-lsd <k9514833133@gmail.com>
2025-03-20 12:49:30 +0300

    TestProject: First part of counting file stat

---
+++ src/main/java/version/third/partfirst/WordCounterService.java
@@ -0,0 +1,25 @@
+package main.java.version.third.partfirst;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Сервис для подсчета слов в файле
+ *
+ */
+public class WordCounterService {
+
+    // приватный конструктор для запрета создания экземпляров
+    private WordCounterService() {
+    }
+
+    public static int countWords (Path filePath) throws IOException {
+        String content = Files.readString(filePath);
+        String[] words = content.toLowerCase()
+                .replaceAll("[^a-zа-я0-9\\s]", "")
+                .trim()
+                .split("\\s+");
+        return words.length;
+    }
+}
